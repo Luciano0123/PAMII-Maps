b29caa94e364de3412b470b2a26a262917edebc0
Pedro e Luciano <pedro@gmail.com>
2024-09-17 12:40:50 -0300

    Maps API

---
+++ Maps/Maps/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Maps;
+namespace Maps
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var location = new Location(-23.51932790342499, -46.59672490400843);
+            var mapSpan = new MapSpan(location, 0.01, 0.01);
+            map.MoveToRegion(mapSpan);
+        }
+
+    }
+
+}
